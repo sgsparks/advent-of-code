d1f0e0d79b39ba9f87660fbec342411bd7b052cc
sam <sgsparks@gmail.com>
2024-01-15 19:22:22 -0800

    MIN STACk

---
+++ Neat-Code/minStack.js
@@ -0,0 +1,41 @@
+var MinStack = function() {
+  this.stack = []
+  this.min = []
+};
+
+/**
+* @param {number} val
+* @return {void}
+*/
+MinStack.prototype.push = function(val) {
+
+  if(this.stack.length === 0) {
+      this.min.push(val)
+  } else {
+      let minimum = Math.min(val, this.min[this.min.length -1])
+      this.min.push(minimum)
+  }
+      this.stack.push(val)
+};
+
+/**
+* @return {void}
+*/
+MinStack.prototype.pop = function() {
+  this.stack.pop()
+  this.min.pop()
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.top = function() {
+  return this.stack[this.stack.length -1]
+};
+
+/**
+* @return {number}
+*/
+MinStack.prototype.getMin = function() {
+  return this.min[this.min.length -1]
+};
